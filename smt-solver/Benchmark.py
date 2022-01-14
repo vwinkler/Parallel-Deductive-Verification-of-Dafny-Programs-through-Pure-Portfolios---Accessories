c6529669aad08ba19e7124fe06b1c46e824e77f4
Vincent Winkler <vincent.winkler@student.kit.edu>
2022-01-14 18:04:02 +0100

    Refactor portfolio solver

---
+++ smt-solver/Benchmark.py
@@ -0,0 +1,89 @@
+import os
+from time import time
+
+from SmtPortfolio import SmtPortfolio
+from SmtProcess import SmtProcess
+import util
+
+
+class Benchmark:
+    def __init__(self, output):
+        self.output = output
+        self.problem = None
+        self.start_time = None
+        self.end_time = None
+        self.processes = None
+        self.portfolio = None
+
+    def run(self, problem):
+        self.problem = problem
+        self.start_time = time()
+        self.runPortfolio()
+        self.end_time = time()
+        return self.makeResultJson()
+
+    def runPortfolio(self):
+        self.portfolio = SmtPortfolio(self.problem)
+        self.startProcesses()
+        self.awaitTerminationOfAllProcesses()
+
+    def startProcesses(self):
+        self.processes = dict()
+        instances = self.portfolio.make_instances(4)
+        for instance in instances:
+            self.startProcess(instance)
+
+    def startProcess(self, instance):
+        command = instance.get_cli_command()
+        process = SmtProcess(command)
+        pid = process.get_pid()
+        self.processes[pid] = (process, instance)
+        self.printStartMessage(command, pid)
+        instance.assign_process(process.get_stream())
+
+    def awaitTerminationOfAllProcesses(self):
+        while len(self.processes) > 0:
+            pid = self.awaitTerminatedProcess()
+            if pid in self.processes:
+                self.cleanUpTerminatedProcess(pid)
+
+    def awaitTerminatedProcess(self):
+        try:
+            pid = self.awaitNextProcessTermination()
+        except ChildProcessError:
+            pid = self.awaitTerminationOfSomeProcess()
+        return pid
+
+    def awaitNextProcessTermination(self):
+        pid, _ = os.wait()
+        return pid
+
+    def awaitTerminationOfSomeProcess(self):
+        pid = next(iter(self.processes.keys()))
+        (process, instance) = self.processes[pid]
+        process.wait()
+        return pid
+
+    def cleanUpTerminatedProcess(self, pid):
+        (process, instance) = self.processes[pid]
+        del self.processes[pid]
+        instance.unassign_process()
+        self.printTerminationMessage(pid)
+        for pid, (process, instance) in self.processes.items():
+            if instance.has_termination_pending():
+                process.terminate()
+
+    def makeResultJson(self):
+        result = {
+            "portfolio": self.portfolio.get_result(),
+            "total_runtime": util.format_timediff(self.start_time, self.end_time)
+        }
+        return result
+
+    def printStartMessage(self, command, pid):
+        message = "\trunning '{}' on pid={} (total: {} processes)\n".format(" ".join(command), pid, len(self.processes))
+        self.output.write(message)
+
+    def printTerminationMessage(self, pid):
+        message = "\tprocess with pid={} terminated (total: {} processes remaining)\n".format(pid, len(self.processes))
+        self.output.write(message)
--- smt-solver/main.py
+++ smt-solver/main.py
@@ -1,15 +1,12 @@
+import sys
 from io import open
-import os
 import subprocess
 from subprocess import PIPE
 import argparse
 import json
-from time import time
 
 from SmtProblem import SmtProblem
-from SmtPortfolio import SmtPortfolio
-from SmtProcess import SmtProcess
-import util
+from Benchmark import Benchmark
 
 
 def file_tuple(s):
@@ -34,39 +31,8 @@ if __name__ == "__main__":
         in_file = open(problem_files["in"], "r")
         out_file = open(problem_files["out"], "w")
         problem = SmtProblem(in_file)
-        start_time = time()
-        portfolio = SmtPortfolio(problem)
+        benchmark = Benchmark(sys.stdout)
 
-        processes = dict()
-        instances = portfolio.make_instances(4)
-        for instance in instances:
-            command = instance.get_cli_command()
-            process = SmtProcess(command)
-            pid = process.get_pid()
-            processes[pid] = (process, instance)
-            print("\trunning '{}' on pid={} (total: {} processes)".format(" ".join(command), pid, len(processes)))
-            instance.assign_process(process.get_stream())
+        result = benchmark.run(problem)
 
-        while len(processes) > 0:
-            try:
-                pid, _ = os.wait()
-            except ChildProcessError as e:
-                pid = next(iter(processes.keys()))
-                (process, instance) = processes[pid]
-                process.wait()
-
-            if pid in processes:
-                (process, instance) = processes[pid]
-                del processes[pid]
-                instance.unassign_process()
-                print("\tprocess with pid={} terminated (total: {} processes remaining)".format(pid, len(processes)))
-
-                for pid, (process, instance) in processes.items():
-                    if instance.has_termination_pending():
-                        process.terminate()
-        end_time = time()
-        result = {
-            "portfolio": portfolio.get_result(),
-            "total_runtime": util.format_timediff(start_time, end_time)
-        }
         json.dump(result, out_file, indent=2)
