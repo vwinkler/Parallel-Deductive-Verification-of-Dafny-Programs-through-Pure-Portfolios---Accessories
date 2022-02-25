1e902d95901b861f9567a34238afd24317d114ef
Vincent Winkler <vincent.winkler@student.kit.edu>
2022-02-25 20:53:35 +0100

    Refactor portfolio

--- dafnyportfolio/src/Portfolio.py
+++ dafnyportfolio/src/Portfolio.py
@@ -7,47 +7,91 @@ from XmlResultParser import *
 class Portfolio:
     def __init__(self, filename, procedure_name, num_processes, option_selector, dafny_command):
         self.option_selector = option_selector
-        self.num_processes = num_processes
-        self.filename = filename
-        self.procedure_name = procedure_name
-        self.dafny_command = dafny_command
+        self.dafny_instance_factory = DafnyInstanceFactory(dafny_command, filename, procedure_name)
         self.process_collection = ProcessCollection()
+        self.xml_parser = XmlResultParser()
+        
+        dynamic_args = self.select_dynamic_args(num_processes)
+        self.instances = self.create_dafny_instances(dynamic_args)
 
     def run(self):
-        proc_argument = "/proc:*{}".format(self.procedure_name)
+        self.start_dafny_instances(self.instances)
+        self.wait_for_termination()
+        return self.collect_results(self.instances)
 
-        dynamic_args_selection = self.option_selector.create_arguments(self.num_processes)
-        cmds = dict()
-        output_files = [NamedTemporaryFile("w+b", suffix=".out") for i in range(self.num_processes)]
-        error_files = [NamedTemporaryFile("w+b", suffix=".err") for i in range(self.num_processes)]
-        xml_files = [NamedTemporaryFile("r", suffix=".xml") for i in range(self.num_processes)]
-        for id in range(self.num_processes):
-            static_args = [self.dafny_command, self.filename, proc_argument, "/xml:{}".format(xml_files[id].name)]
-            dynamic_args = dynamic_args_selection[id]
-            cmds[id] = static_args + dynamic_args
-            self.process_collection.start_process(cmds[id], stdout=output_files[id], stderr=error_files[id])
+    def select_dynamic_args(self, num_processes):
+        return self.option_selector.create_arguments(num_processes)
 
+    def create_dafny_instances(self, dynamic_args_selection):
+        instances = dict()
+        for id, dynamic_args in enumerate(dynamic_args_selection):
+            instances[id] = self.dafny_instance_factory.create_dafny_instance(dynamic_args)
+        return instances
+
+    def start_dafny_instances(self, instances):
+        for instance in instances.values():
+            instance.start(self.process_collection)
+
+    def wait_for_termination(self):
         while self.process_collection.count_running_processes() > 0:
             self.process_collection.await_termination_of_any_process()
 
-        xml_parser = XmlResultParser()
+    def collect_results(self, instances):
         instances_results = []
-        for id in range(self.num_processes):
-            output_files[id].seek(0)
-            error_files[id].seek(0)
-            xml_files[id].seek(0)
-
-            try:
-                xml_results = xml_parser.parse(xml_files[id])
-            except Exception as e:
-                xml_results = str(e)
-
+        for id, instance in instances.items():
             instance_results = {"id": id,
-                                "cmd": cmds[id],
-                                "diversification": dynamic_args_selection[id],
-                                "stdout": output_files[id].read().splitlines(),
-                                "stderr": error_files[id].read().splitlines(),
-                                "xml": xml_results}
+                                "cmd": instance.get_command(),
+                                "diversification": instance.get_dynamic_args(),
+                                "stdout": instance.open_output_file().read().splitlines(),
+                                "stderr": instance.open_error_file().read().splitlines(),
+                                "xml": self.try_read_xml(instance)}
             instances_results.append(instance_results)
-
         return instances_results
+
+    def try_read_xml(self, instance):
+        try:
+            xml_results = self.xml_parser.parse(instance.open_xml_file())
+        except Exception as e:
+            xml_results = str(e)
+        return xml_results
+
+
+class DafnyInstanceFactory:
+    def __init__(self, dafny_command, dfy_filename, procedure_name):
+        self.dafny_command = dafny_command
+        self.dfy_filename = dfy_filename
+        self.procedure_name = procedure_name
+
+    def create_dafny_instance(self, dynamic_args):
+        return DafnyInstance(self.dafny_command, self.dfy_filename, self.procedure_name, dynamic_args)
+
+
+class DafnyInstance:
+    def __init__(self, dafny_command, dfy_filename, procedure_name, dynamic_args):
+        self._output_file = NamedTemporaryFile("w+b", suffix=".out")
+        self._error_file = NamedTemporaryFile("w+b", suffix=".err")
+        self._xml_file = NamedTemporaryFile("r", suffix=".xml")
+
+        proc_argument = "/proc:*{}".format(procedure_name)
+        static_args = [dafny_command, dfy_filename, proc_argument]
+        xml_argument = "/xml:{}".format(self._xml_file.name)
+        self._dynamic_args = dynamic_args
+        self._cmd = static_args + [xml_argument] + self._dynamic_args
+
+    def start(self, process_collection):
+        return process_collection.start_process(self._cmd, stdout=self._output_file, stderr=self._error_file)
+
+    def get_command(self):
+        return self._cmd
+
+    def get_dynamic_args(self):
+        return self._dynamic_args
+
+    def open_xml_file(self):
+        return open(self._xml_file.name, "rb")
+
+    def open_output_file(self):
+        return open(self._output_file.name, "rb")
+
+    def open_error_file(self):
+        return open(self._error_file.name, "rb")
