89b1331e8253a3fc69a1586cd41e68e95d2cff92
Vincent Winkler <vincent.winkler@student.kit.edu>
2022-02-26 16:33:32 +0100

    Make script to run benchmarks customizable

--- evaluation/dafny/seed_diversification/make_benchmark_calls.py
+++ evaluation/dafny/seed_diversification/make_benchmark_calls.py
@@ -1,10 +1,18 @@
+import argparse
+
 script = "../../../dafnyportfolio/src/solve.py"
 methods_file = "methods_to_benchmark.txt"
 benchmark_path_prefix = "../../../benchmarks/dafny/"
-command_selection = "seed-diversification"
-with open(methods_file) as file:
-    for line in file.readlines():
-        dfy_filename, method_name = line.split()
-        cmd_template = "(set -x; python {scr} {path}{dfy} {method} {div} {dfy}.json)"
-        print(cmd_template.format(scr=script, path=benchmark_path_prefix, dfy=dfy_filename, method=method_name,
-                                  div=command_selection))
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Create calls to portfolio solver")
+    parser.add_argument(metavar="OPTIONSELECTOR", dest="command_selection", type=str)
+    parser.add_argument(metavar="ARGS", dest="additional_args", type=str, nargs="?", default="")
+    args = parser.parse_args()
+
+    with open(methods_file) as file:
+        for line in file.readlines():
+            dfy_filename, method_name = line.split()
+            cmd_template = "(set -x; python {scr} {path}{dfy} {method} {div} {dfy}.json {args})"
+            print(cmd_template.format(scr=script, path=benchmark_path_prefix, dfy=dfy_filename, method=method_name,
+                                      div=args.command_selection, args=args.additional_args))
