1786e41db24ac9a48cd8a2ad8363ff58ad061760
Vincent Winkler <vincent.winkler@student.kit.edu>
2022-02-28 16:40:10 +0100

    Add multiple seeds to run script

--- results/dafny/make_all_benchmark_calls.sh
+++ results/dafny/make_all_benchmark_calls.sh
@@ -1,11 +1,16 @@
-#/bin/sh
+#!/bin/sh
 
 cmd="python make_benchmark_calls.py"
 
-echo "mkdir -p no-portfolio && cd no-portfolio"
-$cmd no-diversification "--num-threads 1"
-echo "cd .."
+seeds="286325 752764 522591 842099 811175"
 
-echo "mkdir -p seed-diversification && cd seed-diversification"
-$cmd seed-diversification "--num-threads 4"
-echo "cd .."
+echo $seeds
+for seed in $seeds; do
+  echo "mkdir -p no-portfolio && cd no-portfolio"
+  $cmd no-diversification "$seed" "--num-threads 1 --seed=$seed"
+  echo "cd .."
+
+  echo "mkdir -p seed-diversification && cd seed-diversification"
+  $cmd seed-diversification "_$seed" "--num-threads 4 --seed=$seed"
+  echo "cd .."
+done
--- results/dafny/make_benchmark_calls.py
+++ results/dafny/make_benchmark_calls.py
@@ -7,12 +7,15 @@ benchmark_path_prefix = "../../../benchmarks/dafny/"
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Create calls to portfolio solver")
     parser.add_argument(metavar="OPTIONSELECTOR", dest="command_selection", type=str)
+    parser.add_argument(metavar="RESULTSFILESUFFIX", dest="results_file_suffix", type=str)
     parser.add_argument(metavar="ARGS", dest="additional_args", type=str, nargs="?", default="")
     args = parser.parse_args()
 
     with open(methods_file) as file:
         for line in file.readlines():
             dfy_filename, method_name = line.split()
-            cmd_template = "(set -x; python {scr} {path}{dfy} {method} {div} {dfy}.json {args})"
+            results_filename = "{}_{}.json".format(dfy_filename, args.results_file_suffix)
+            cmd_template = "(set -x; python {scr} {path}{dfy} {method} {div} {results_file} {args})"
             print(cmd_template.format(scr=script, path=benchmark_path_prefix, dfy=dfy_filename, method=method_name,
-                                      div=args.command_selection, args=args.additional_args))
+                                      div=args.command_selection, args=args.additional_args,
+                                      results_file=results_filename))
