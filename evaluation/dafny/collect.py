726b560cc7611b7224da7ad7fc6b1169b38a85d6
Vincent Winkler <vincent.winkler@student.kit.edu>
2022-03-01 00:40:30 +0100

    Add scripts to (box)plot results (#3)

---
+++ evaluation/dafny/collect.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import json
+
+
+# Returned columns: problem | procedure | option_selector | processes | seed | runtime
+def collect_runtimes(result_filenames) -> pd.DataFrame:
+    df = pd.DataFrame()
+
+    for filename in result_filenames:
+        with open(filename) as file:
+            results = json.load(file)
+        data = pd.DataFrame({"problem": [results["args"]["dafny_file"]],
+                             "procedure": [results["args"]["procedure_name"]],
+                             "option_selector": [(results["args"]["option_selector_name"])],
+                             "processes": [len(results["instances"])],
+                             "seed": [results["args"]["seed"]],
+                             "runtime": [results["total_runtime"]["duration"]]})
+        data = data.astype({"runtime": "float64"})
+        df = pd.concat([df, data])
+
+    return df
---
+++ evaluation/dafny/make_plots.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+python make_runtime_boxplot.py ../../results/dafny/no-portfolio/*.json ../../results/dafny/seed-diversification/*.json
