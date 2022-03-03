1bfff23aed0f5e66be6bee954b24a15244beb5af
Vincent Winkler <vincent.winkler@student.kit.edu>
2022-03-03 15:47:22 +0100

    Add results and evaluate

--- evaluation/dafny/make_plots.sh
+++ evaluation/dafny/make_plots.sh
@@ -4,4 +4,6 @@ python make_runtime_boxplot.py \
     ../../results/dafny/no-diversification/*.json \
     ../../results/dafny/seed-diversification/*.json \
     ../../results/dafny/sat-seed-diversification/*.json \
+    ../../results/dafny/split-limit-diversification/*.json \
+    ../../results/dafny/dynamic-split-limit-diversification/*.json \
     
