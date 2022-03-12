1976a3983853a88c197706f3198640d709f2037d
Vincent Winkler <vincent.winkler@student.kit.edu>
2022-03-13 00:31:30 +0100

    Refactor make_job.py

--- dafnyportfolio/src/job_creation/make_job.py
+++ dafnyportfolio/src/job_creation/make_job.py
@@ -1,35 +1,7 @@
 import argparse
-import json
 import os.path
-import sys
 
-from slugify import slugify
-import expand
-
-
-def turn_key_errors_and_null_to_emptystring(format_string, container, key):
-    if key in container and container[key] is not None:
-        return format_string.format(container[key])
-    else:
-        return ""
-
-
-def turn_null_to_emptystring(format_string, value):
-    if value is None:
-        return ""
-    else:
-        format_string.format(value)
-
-
-def make_result_filename(call):
-    return slugify(" ".join(make_arg_strings(call))) + ".json"
-
-
-def make_arg_strings(call):
-    result_file_identifier = [call['dfy-path'], call['procedurename'], call['optionselector'],
-                              call['optionselector'], call['num_instances'], call['only_instances'],
-                              call['seed']]
-    return [str(v) for v in result_file_identifier]
+from util import *
 
 
 def make_dfy_path(base, rest):
@@ -37,6 +9,25 @@ def make_dfy_path(base, rest):
         return rest
     else:
         return os.path.join(base, rest)
+
+
+def make_self_announcing_normal_command(args, call, results_filename):
+    return "(set -x; {})".format(make_normal_command(args, call, results_filename))
+
+
+def make_normal_command(args, call, results_filename):
+    cmd = ("python "
+           f"{args.command} "
+           f"{make_dfy_path(args.dfy_base_path, call['dfy-path'])} "
+           f"{call['procedurename']} "
+           f"{call['optionselector']} "
+           f"{results_filename} "
+           )
+    cmd += turn_key_errors_and_null_to_emptystring("--num-instances {} ", call, "num_instances")
+    cmd += turn_key_errors_and_null_to_emptystring("--seed {} ", call, "seed")
+    cmd += turn_key_errors_and_null_to_emptystring("--only-instances {} ", call, "only_instances")
+    cmd += turn_null_to_emptystring("--dafny-cmd {} ", args.dafny_cmd)
+    return cmd
 
 
 if __name__ == '__main__':
@@ -48,30 +39,6 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     print("#!/bin/sh")
-    with open(args.filename) as job_file:
-        job = json.load(job_file)
-        expanded_job = expand.process(job)
 
-        seen_results_filenames = set()
-        for call in expanded_job:
-            results_filename = make_result_filename(call)
-            if results_filename in seen_results_filenames:
-                print("duplicate results file '{}'".format(results_filename), file=sys.stderr)
-            seen_results_filenames.add(results_filename)
-
-            cmd = (""
-                   "(set -x; "
-                   "python "
-                   f"{args.command} "
-                   f"{make_dfy_path(args.dfy_base_path, call['dfy-path'])} "
-                   f"{call['procedurename']} "
-                   f"{call['optionselector']} "
-                   f"{results_filename} "
-                   )
-            cmd += turn_key_errors_and_null_to_emptystring("--num-instances {} ", call, "num_instances")
-            cmd += turn_key_errors_and_null_to_emptystring("--seed {} ", call, "seed")
-            cmd += turn_key_errors_and_null_to_emptystring("--only-instances {} ", call, "only_instances")
-            cmd += turn_null_to_emptystring("--dafny-cmd {} ", args.dafny_cmd)
-
-            cmd += ")"
-            print(cmd)
+    for_all_calls(args.filename,
+                  lambda call, results_file: print(make_self_announcing_normal_command(args, call, results_file)))
---
+++ dafnyportfolio/src/job_creation/util.py
@@ -0,0 +1,47 @@
+import json
+import sys
+from slugify import slugify
+import expand
+
+
+def turn_key_errors_and_null_to_emptystring(format_string, container, key):
+    if key in container and container[key] is not None:
+        return format_string.format(container[key])
+    else:
+        return ""
+
+
+def turn_null_to_emptystring(format_string, value):
+    if value is None:
+        return ""
+    else:
+        format_string.format(value)
+
+
+def make_result_filename(call):
+    return slugify(" ".join(make_arg_strings(call))) + ".json"
+
+
+def make_arg_strings(call):
+    result_file_identifier = [call['dfy-path'], call['procedurename'], call['optionselector'],
+                              call['optionselector'], call['num_instances'], call['only_instances'],
+                              call['seed']]
+    return [str(v) for v in result_file_identifier]
+
+
+def print_error_if_duplicate(seen_filenames, filename):
+    if filename in seen_filenames:
+        print("duplicate results file '{}'".format(filename), file=sys.stderr)
+
+
+def for_all_calls(filename, f):
+    with open(filename) as job_file:
+        job = json.load(job_file)
+        expanded_job = expand.process(job)
+
+        seen_results_filenames = set()
+        for call in expanded_job:
+            results_filename = make_result_filename(call)
+            print_error_if_duplicate(seen_results_filenames, results_filename)
+            seen_results_filenames.add(results_filename)
+            f(call, results_filename)
