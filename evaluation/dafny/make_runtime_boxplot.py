32000868b3a201876d3e907bece6882a03dfbd6c
Vincent Winkler <vincent.winkler@student.kit.edu>
2022-03-21 23:35:33 +0100

    Fix plot filenames

--- evaluation/dafny/make_runtime_boxplot.py
+++ evaluation/dafny/make_runtime_boxplot.py
@@ -28,14 +28,14 @@ if __name__ == '__main__':
     grouped_data = df.groupby(["problem", "procedure", "num_instances", "is_portfolio"])
     for (problem, procedure, num_instances, is_portfolio), group in grouped_data:
         y_max = group["runtime"].max()
-        group = group.pivot(columns=["option_selector", "num_instances", "only_instances"], index="seed",
-                            values="runtime").reset_index()
+        group = group.pivot(columns=["option_selector", "only_instances"], index="seed", values="runtime").reset_index()
         boxplot = group.plot(kind="box", ylim=(0, y_max * 1.1))
 
         plt.setp(boxplot.xaxis.get_majorticklabels(), rotation=90)
 
         cmp_string = "compare_portfolios" if is_portfolio else "compare_instances"
-        filename = "{}/{}_{}_{}{}.svg".format(cmp_string, slugify(problem), slugify(procedure), slugify(num_instances),
+        filename = "{}/{}_{}_{}{}.svg".format(cmp_string, slugify(problem), slugify(procedure), num_instances,
                                               args.out_file_suffix)
+        print(filename)
         save_plot(boxplot, filename)
         plt.close()
