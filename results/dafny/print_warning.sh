dd1feed3baf94ba8f65baf3ab026dbaa8ea9503d
Vincent Winkler <vincent.winkler@student.kit.edu>
2022-04-28 12:46:51 +0200

    Warn to make *.bpl files in run scripts

--- results/dafny/print_warning.sh
+++ results/dafny/print_warning.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-printf "\033[1;33mMake sure you are using the correct container version\033[0m"
-sleep "0.5s"
+printf "\033[1;33mMake sure you are using the correct container version\033[0m\n"
+printf "\033[1;33mMake sure you have converted benchmarks (*.dfy to *.bpl)\033[0m"
+sleep "1s"
 printf "\n."
-sleep "0.5s"
+sleep "1s"
 printf "."
-sleep "0.5s"
+sleep "1s"
 printf "."
-sleep "0.5s"
+sleep "1s"
 printf "\n"
 
