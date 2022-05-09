0e1e6f898bbe561cf666cd4fa13ab59fd1c7728e
Vincent Winkler <vincent.winkler@student.kit.edu>
2022-05-10 00:59:51 +0200

    Penalize results that do not have 'outcome=correct'

--- evaluation/dafny/penalize_runtimes.py
+++ evaluation/dafny/penalize_runtimes.py
@@ -1,6 +1,7 @@
 def penalize(df, max_runtime=600, penalty_runtime=1200):
     penalize_overtime(df, max_runtime, penalty_runtime)
     penalize_unfinished(df, max_runtime, penalty_runtime)
+    penalize_incorrect(df, max_runtime, penalty_runtime)
 
 
 def penalize_overtime(df, max_runtime=600, penalty_runtime=1200):
@@ -9,3 +10,7 @@ def penalize_overtime(df, max_runtime=600, penalty_runtime=1200):
 
 def penalize_unfinished(df, max_runtime=600, penalty_runtime=1200):
     df["runtime"] = df.apply(lambda row: penalty_runtime if not row["finished"] else row["runtime"], axis=1)
+
+
+def penalize_incorrect(df, max_runtime, penalty_runtime):
+    df["runtime"] = df.apply(lambda row: penalty_runtime if not row["correct"] else row["runtime"], axis=1)
