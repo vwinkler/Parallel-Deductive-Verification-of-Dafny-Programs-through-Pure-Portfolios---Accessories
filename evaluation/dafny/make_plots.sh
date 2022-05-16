4ec2e11aa9e8bf7f872969167a8c62402e3f3625
Vincent Winkler <vincent.winkler@student.kit.edu>
2022-05-16 16:55:58 +0200

    Evaluate results of benchmarks without assertions

--- evaluation/dafny/make_plots.sh
+++ evaluation/dafny/make_plots.sh
@@ -2,3 +2,10 @@
 
 find ../../results/dafny/{portfolio_assembling,bool_params}/ -name '*.json' |
 python make_matrix.py -
+
+(
+mkdir -p noassert &&
+cd noassert &&
+find ../../../results/dafny/noassert/ -name '*.json' |
+    python ../make_matrix.py -
+)
