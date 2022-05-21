5ee9897b632c53fe52c5db17767e5765944bea2a
Vincent Winkler <vincent.winkler@student.kit.edu>
2022-05-21 18:15:24 +0200

    Refactor evaluation

--- evaluation/dafny/make_matrix.py
+++ evaluation/dafny/make_matrix.py
@@ -2,10 +2,24 @@ import matplotlib.pyplot as plt
 from matplotlib import rcParams
 from collect import *
 from collection_persistence import load_collection
-from find_empty_matrix_cells import *
-from penalize_runtimes import *
-from util import save_plot, ensure_surrounding_directory_exists
+from finding_empty_matrix_cells import find_empty_cells
+from runtime_penalization import *
+from util import *
 import seaborn
+
+
+def warn_about_empty_cells(df):
+    for cell in sorted(find_empty_cells(df), key=lambda cell: cell.row_index):
+        warn_about_empty_cell(cell)
+
+
+def warn_about_empty_cell(cell):
+    index = cell.column_index
+    configuration = cell.column_name
+    (benchmark_file, benchmark_method) = cell.row_name
+    print((f"Runtime for {benchmark_file} {benchmark_method} (row {cell.row_index}) "
+           f"with {configuration} (column {index}) "
+           f"is empty"))
 
 
 def plot(relative_df, title, vmax):
@@ -23,70 +37,59 @@ def plot(relative_df, title, vmax):
     return heatmap
 
 
-def turn_into_benchmark_x_config_matrix(df):
-    df = df.copy()
-    df = df[df["num_running_instances"] == 1]
-    df["configuration"] = df[("diversification", 0)]
-    df["score"] = df["runtime"]
-    df = df[["configuration", "problem", "procedure", "score"]]
-    df = df.groupby(["configuration", "problem", "procedure"], as_index=False).agg("mean")
-    df = df.pivot(index=["problem", "procedure"], columns="configuration", values="score")
-    df["[vbs]"] = df.min(axis=1)
-    df.loc["[total]"] = df.sum()
-    return df
+def export_to_html(filename, df):
+    with open(filename, "w") as f:
+        df.to_html(f)
 
 
-def warn_about_empty_cells(df):
-    for cell in sorted(find_empty_cells(df), key=lambda cell: cell.row_index):
-        warn_about_empty_cell(cell)
+def export_to_ods(filename, df):
+    with open(filename, "wb") as f:
+        try:
+            df.to_excel(f, engine="odf")
+        except ModuleNotFoundError as e:
+            print(f"Could not save as '{f.name}': {e}")
 
 
-def warn_about_empty_cell(cell):
-    index = cell.column_index
-    configuration = cell.column_name
-    (benchmark_file, benchmark_method) = cell.row_name
-    print((f"Runtime for {benchmark_file} {benchmark_method} (row {cell.row_index}) "
-           f"with {configuration} (column {index}) "
-           f"is empty"))
+def export_axis_labels(filename, df):
+    ensure_surrounding_directory_exists(filename)
+    with open(filename, "w") as f:
+        f.write("\n".join([f"{k}\t{r}" for k, r in enumerate(df.index)]))
+        f.write("\n\n")
+        f.write("\n".join([f"{k}\t{c}" for k, c in enumerate(df.columns)]))
 
 
-if __name__ == '__main__':
+def plot_as_heatmap(filename, relative_df, max_value, title):
+    rcParams.update({'figure.autolayout': True})
+    relative_heatmap = plot(relative_df, title, max_value)
+    save_plot(relative_heatmap, filename)
+    plt.close()
+
+
+def main():
     parser = argparse.ArgumentParser(description="Plot matrix displaying runtime")
     parser.add_argument(metavar="COLLECTION", dest="results_collection", type=str)
     parser.add_argument("--target-dir", dest="target_dir", type=str, default=".")
     args = parser.parse_args()
 
     df = load_collection(args.results_collection)
-
     penalize_results_table(df)
-    df = turn_into_benchmark_x_config_matrix(df)
+    df = make_matrix(df)
     warn_about_empty_cells(df)
-
     penalize_runtime_matrix(df)
 
-    find_empty_cells(df)
-    relative_df = df.apply(lambda row: row - row.min(), axis=1)
+    relative_matrix = df.apply(lambda row: row - row.min(), axis=1)
 
-    rcParams.update({'figure.autolayout': True})
     basename = f"{args.target_dir}/01"
+
     print(f"{basename}.svg")
-    ensure_surrounding_directory_exists(basename)
-    with open(f"{basename}.txt", "w") as f:
-        f.write("\n".join([f"{k}\t{r}" for k, r in enumerate(df.index)]))
-        f.write("\n\n")
-        f.write("\n".join([f"{k}\t{c}" for k, c in enumerate(df.columns)]))
-    heatmap = plot(df, "Runtime in Seconds", 1200)
-    save_plot(heatmap, f"{basename}.svg")
-    plt.close()
-    relative_heatmap = plot(relative_df, "Runtime Difference to VBS in Seconds", 10)
-    save_plot(relative_heatmap, f"{basename}_relative.svg")
-    plt.close()
 
-    with open(f"{basename}.html", "w") as f:
-        df.to_html(f)
+    export_axis_labels(f"{basename}.txt", df)
+    plot_as_heatmap(f"{basename}.svg", df, 1200, "Runtime in Seconds")
+    plot_as_heatmap(f"{basename}_relative.svg", relative_matrix, 10, "Runtime Difference to VBS in Seconds")
 
-    with open(f"{basename}.ods", "wb") as f:
-        try:
-            df.to_excel(f, engine="odf")
-        except ModuleNotFoundError as e:
-            print(f"Could not save as '{f.name}': {e}")
+    export_to_html(f"{basename}.html", df)
+    export_to_ods(f"{basename}.ods", df)
+
+
+if __name__ == '__main__':
+    main()
--- evaluation/dafny/util.py
+++ evaluation/dafny/util.py
@@ -11,3 +11,18 @@ def ensure_surrounding_directory_exists(filename):
         os.makedirs(os.path.dirname(filename))
     except FileExistsError:
         pass
+
+
+def make_matrix(df):
+    df = df.copy()
+    df = df[df["num_running_instances"] == 1]
+    df["configuration"] = df[("diversification", 0)]
+    df["score"] = df["runtime"]
+    df = df[["configuration", "problem", "procedure", "score"]]
+    df = df.groupby(["configuration", "problem", "procedure"], as_index=False).agg("mean")
+    df = df.pivot(index=["problem", "procedure"], columns="configuration", values="score")
+    df["[vbs]"] = df.min(axis=1)
+    df.loc["[total]"] = df.sum()
+    return df
+
+
