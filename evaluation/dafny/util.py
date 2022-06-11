16403e1e0a58e85083b6942d07dc6890e8ad1ed5
Vincent Winkler <vincent.winkler@student.kit.edu>
2022-06-11 18:20:55 +0200

    Add 'num_running_instances' to matrices

--- evaluation/dafny/util.py
+++ evaluation/dafny/util.py
@@ -15,14 +15,11 @@ def ensure_surrounding_directory_exists(filename):
 
 def make_matrix(df):
     df = df.copy()
-    # df = df[df["num_running_instances"] == 1]
-    # df["configuration", "first"] = df[("diversification", 0)]  # todo: work on this
-    # df["configuration", "second"] = df[("diversification", 1)]  # todo: work on this
-    df["configuration"] = df["diversification_string"]  # todo: work on this
+    df["configuration"] = df["diversification_string"]
     df["score"] = df["runtime"]
-    df = df[["configuration", "problem", "procedure", "score"]]
-    df = df.groupby(["configuration", "problem", "procedure"], as_index=False).agg("mean")
-    df = df.pivot(index=["problem", "procedure"], columns="configuration", values="score")
+    df = df[["configuration", "num_running_instances", "problem", "procedure", "score"]]
+    df = df.groupby(["configuration", "num_running_instances", "problem", "procedure"], as_index=False).agg("mean")
+    df = df.pivot(index=["problem", "procedure"], columns=["configuration", "num_running_instances"], values="score")
     df["[vbs]"] = df.min(axis=1)
     df.loc["[total]"] = df.sum()
     return df
