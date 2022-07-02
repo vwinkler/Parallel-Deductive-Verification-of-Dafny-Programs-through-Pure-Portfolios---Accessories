0d24d7a13e400f5134f88f552c971c7a1d9c4406
Vincent Winkler <vincent.winkler@student.kit.edu>
2022-07-02 23:01:34 +0200

    Adjust cactus plot

--- dafnyportfolio/src/evaluation/plot_n_vcs_cactus.py
+++ dafnyportfolio/src/evaluation/plot_n_vcs_cactus.py
@@ -20,7 +20,9 @@ class Main:
     def run(self):
         df = load_collection(self.args.results_collection_in)
         df["num_cpus"] = self.make_num_cpus_column(df)
-        df["vcs_max_keep_going_splits"] = self.make_vcsmaxkeepgoingsplits_column(df)
+        df["vcs_maxsplits"] = self.make_vcsmaxsplitscolumn_column(df)
+        df["vcs_split_to_p_ratio"] = df["vcs_maxsplits"] // df["num_cpus"]
+        print(df[["num_cpus", "vcs_maxsplits", "vcs_split_to_p_ratio"]].to_string())
         df = self.filter_best_vcs_config(df)
         df = self.filter_by_parallelity(df)
         df = self.accumulate_iterations(df)
@@ -30,38 +32,42 @@ class Main:
         self.plot(runtime_chart, x_max)
 
     def filter_by_parallelity(self, df):
-        return df[df["num_cpus"] <= 4]
+        return df[df["num_cpus"].isin([1, 2, 4, 8])]
 
     def filter_best_vcs_config(self, df):
-        return df[(df["source"] == "more_iterations") | (df["vcs_max_keep_going_splits"] == 8)]
+        return df[(df["source"] == "more_iterations") | (df["vcs_split_to_p_ratio"] == 1)]
 
     def make_num_cpus_column(self, df):
         def get_num_cpus(row):
             if row["source"] == "more_iterations":
                 return row["num_running_instances"]
             elif row["source"] == "parallel_vcs":
-                match = re.search("/vcsCores:(\d+)", row["diversification_string"])
+                diversification_string = row["diversification_string"]
+                match = re.search("/vcsCores:(\d+)", diversification_string)
                 if not match:
-                    raise RuntimeError("Illegal diversification_string for source='parallel_vcs'")
+                    error = f"Illegal diversification string '{diversification_string}' for source='parallel_vcs'"
+                    raise RuntimeError(error)
                 return int(match[1])
             else:
                 raise RuntimeError("Unknown source")
 
         return df.apply(get_num_cpus, axis="columns")
 
-    def make_vcsmaxkeepgoingsplits_column(self, df):
-        def get_vcsmaxkeepgoingsplits(row):
+    def make_vcsmaxsplitscolumn_column(self, df):
+        def get_vcsmaxsplits(row):
             if row["source"] == "more_iterations":
                 return -1
             elif row["source"] == "parallel_vcs":
-                match = re.search("/vcsMaxKeepGoingSplits:(\d+)", row["diversification_string"])
+                diversification_string = row["diversification_string"]
+                match = re.search("/vcsMaxSplits:(\d+)", diversification_string)
                 if not match:
-                    raise RuntimeError("Illegal diversification_string for source='parallel_vcs'")
+                    error = f"Illegal diversification string '{diversification_string}' for source='parallel_vcs'"
+                    raise RuntimeError(error)
                 return int(match[1])
             else:
                 raise RuntimeError("Unknown source")
 
-        return df.apply(get_vcsmaxkeepgoingsplits, axis="columns")
+        return df.apply(get_vcsmaxsplits, axis="columns")
 
     def accumulate_iterations(self, df):
         group_columns = ["problem", "procedure", "num_cpus", "source"]
@@ -80,12 +86,13 @@ class Main:
         fig, ax = plt.subplots()
         ax.set_xlim(left=0, right=x_max)
 
-        linestyle_by_num_cpus = {1: "solid", 2: "dotted", 3: "dashed", 4: "dashdot"}
+        linestyle_by_num_cpus = {1: (0, (1, 1)), 2: (0, (2, 2)), 4: (0, (4, 4)), 8: (0, (8, 8))}
+        linewidth_by_source = {"parallel_vcs": 0.8, "more_iterations": 0.5}
         color_by_source = {"parallel_vcs": "tab:blue", "more_iterations": "tab:orange"}
 
         for (num_cpus, source), column in runtime_chart.iteritems():
             ax.stairs(column.index, list(column) + [x_max], linestyle=linestyle_by_num_cpus[num_cpus],
-                      color=color_by_source[source], alpha=0.7)
+                      color=color_by_source[source], alpha=1, linewidth=linewidth_by_source[source])
         if self.args.plot_file:
             fig.savefig(self.args.plot_file)
 
