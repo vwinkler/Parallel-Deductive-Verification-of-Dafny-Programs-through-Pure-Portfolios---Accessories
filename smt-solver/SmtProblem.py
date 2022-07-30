da962201526bd5705221ce5591275ce97241379d
Vincent Winkler <vincent.winkler@student.kit.edu>
2022-07-30 02:20:14 +0200

    Remove legacy files

--- smt-solver/SmtProblem.py
+++
@@ -1,7 +0,0 @@
-import json
-
-
-class SmtProblem:
-    def __init__(self, file):
-        problem = json.load(file)
-        self.smt2_string = problem["smt2"]
--- smt-solver/util/logged_z3.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-logfilename="$(dirname '$0')/logfile"
-
-tee $logfilename | z3 -in $@
