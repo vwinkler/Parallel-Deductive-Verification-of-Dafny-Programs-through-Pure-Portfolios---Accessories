f7a0a432ae7ce4826415a549f9fc4e453149243d
Vincent Winkler <vincent.winkler@student.kit.edu>
2022-08-05 01:23:58 +0200

    Set evaluation matrix to medium instead of mean

--- dafnyportfolio/src/evaluation/util.py
+++ dafnyportfolio/src/evaluation/util.py
@@ -18,7 +18,7 @@ def make_matrix(df):
     df["configuration"] = df["diversification_string"]
     df["score"] = df["runtime"]
     df = df[["configuration", "num_running_instances", "problem", "procedure", "score"]]
-    df = df.groupby(["configuration", "num_running_instances", "problem", "procedure"], as_index=False).agg("mean")
+    df = df.groupby(["configuration", "num_running_instances", "problem", "procedure"], as_index=False).agg("median")
     df = df.pivot(index=["problem", "procedure"], columns=["configuration", "num_running_instances"], values="score")
     df["[vbs]"] = df.min(axis=1)
     df.loc["[total]"] = df.sum()
