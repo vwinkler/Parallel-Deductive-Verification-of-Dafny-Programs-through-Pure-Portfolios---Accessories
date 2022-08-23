61364f745bf71f54260403610762dd9cd718c675
Vincent Winkler <vincent.winkler@student.kit.edu>
2022-08-24 00:51:46 +0200

    Adjust (calculated) n-vbs plots to base on multiple iterations

--- dafnyportfolio/src/evaluation/vbs.py
+++ dafnyportfolio/src/evaluation/vbs.py
@@ -3,7 +3,9 @@ import itertools
 
 def find_vbs_results(df):
     df = df.loc[df["num_running_instances"] == 1].reset_index()
-    top_config_df = df.loc[df.groupby(["problem", "procedure"])["runtime"].idxmin()]
+    group_columns = ["problem", "procedure", "num_running_instances", "diversification_string"]
+    df_agg = df.groupby(group_columns).agg({"runtime": "median"}).reset_index()
+    top_config_df = df_agg.loc[df_agg.groupby(["problem", "procedure"])["runtime"].idxmin()]
     top_config_df.sort_values(["problem", "procedure"], ascending=True)
     return top_config_df
 
