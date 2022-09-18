40b4bf594c316b89652fc9c24cb7ca2182949360
Vincent Winkler <vincent.winkler@student.kit.edu>
2022-09-18 19:30:56 +0200

    Add hover text to 'by benchmark' plot

--- dafnyportfolio/src/evaluation/plot_by_benchmark.py
+++ dafnyportfolio/src/evaluation/plot_by_benchmark.py
@@ -1,5 +1,7 @@
 import argparse
+import random
 
+import numpy as np
 from matplotlib import pyplot as plt
 
 import pandas as pd
@@ -23,24 +25,51 @@ class Main:
         df["default_runtime"] = self.make_default_runtime_column(df)
         df["speedup"] = df["default_runtime"] / df["runtime"]
         df = self.filter_results(df)
-        self.plot(df, "benchmark", "speedup")
+        self.plot(df, "benchmark", "speedup", "diversification_string")
 
     def filter_results(self, df):
         if self.args.max_runtime:
             result = df[df["runtime"] < self.args.max_runtime]
         return result
 
-    def plot(self, df, x, y):
+    def plot(self, df, x, y, hover_text):
         plt.rcParams.update({"text.usetex": True})
         figure, ax = plt.subplots()
-        x_values = df[x]
-        y_values = df[y]
-        ax.scatter(x=x_values, y=y_values)
+        x_values = np.array(df[x])
+        y_values = np.array(df[y])
+        configurations = np.array(df[hover_text])
+        scatter_plot = ax.scatter(x=x_values, y=y_values)
         plt.yscale(self.args.y_scale)
         plt.tight_layout()
         if self.args.plot_file:
             figure.savefig(self.args.plot_file)
         else:
+            annot = ax.annotate("", xy=(0, 0), xytext=(20, 20), textcoords="offset points",
+                                bbox=dict(boxstyle="round", fc="w"), arrowprops=dict(arrowstyle="->"))
+
+            def update_annot(ind):
+                pos = scatter_plot.get_offsets()[ind["ind"][0]]
+                annot.xy = pos
+                text = "\n".join([configurations[n] for n in ind["ind"]])
+                text = text.replace("_", "\\_")
+                annot.set_text(text)
+                annot.get_bbox_patch().set_alpha(0.4)
+
+            def hover(event):
+                vis = annot.get_visible()
+                if event.inaxes == ax:
+                    cont, ind = scatter_plot.contains(event)
+                    if cont and len(ind) < 10:
+                        update_annot(ind)
+                        annot.set_visible(True)
+                        figure.canvas.draw_idle()
+                        ax.set(title=f"{random.choice([1, 2, 3])}")
+                    else:
+                        if vis:
+                            annot.set_visible(False)
+                            figure.canvas.draw_idle()
+
+            figure.canvas.mpl_connect("motion_notify_event", hover)
             plt.show()
 
     def make_default_runtime_column(self, df):
